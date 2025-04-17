c06ef497322a3d0aeb7a518ccdb72354cbd33ff2
AIK0T14P <154295989+AIK0T14P@users.noreply.github.com>
2025-04-17 11:45:03 -0300

    Actualizar code.lua

--- code.lua
+++ code.lua
@@ -1799,7 +1799,7 @@ end
 -- Características actualizadas (removidas las que no funcionan)
 local MovementFeatures = {
     {name = "Fly", callback = ToggleFly},
-    {name = "Speed", callback = ToggleSpeed, slider = true, min = 16, max = 200, default = 16},
+    {name = "Speed", callback = ToggleSpeed, slider = true, min = 16, max = 500, default = 16},
     {name = "SuperJump", callback = ToggleSuperJump, slider = true, min = 50, max = 500, default = 50},
     {name = "InfiniteJump", callback = InfiniteJump},
     {name = "NoClip", callback = NoClip},
