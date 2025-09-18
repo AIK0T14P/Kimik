f3161c44f7162aa1e2b5baace9b7c87e5073b6b0
AIK0T14P <154295989+AIK0T14P@users.noreply.github.com>
2025-09-18 16:35:21 -0300

    Actualizar code.lua

--- code.lua
+++ code.lua
@@ -43,7 +43,7 @@ HumanoidRootPart = HumanoidRootPart:WaitForChild("HumanoidRootPart")
 local EnabledFeatures = {}
 
 -- Sistema de idiomas (ahora por defecto en español)
-lcal Languages = {
+local Languages = {
     ["Español"] = {
         categories = {
             Movement = "Movimiento",
